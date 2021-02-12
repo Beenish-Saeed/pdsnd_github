9ab470aa18977d3fa1b0f69bd5b5da5d3da83590
Beenish-Saeed <beenish.bhs@gmail.com>
2021-02-12 16:29:35 +0500

    Updated bikeshare.py
    The bikeshare.py is updated with a minor change of increasing dashes.

--- bikeshare.py
+++ bikeshare.py
@@ -36,7 +36,7 @@ def get_filters():
     # get user input for day of week (all, monday, tuesday, ... sunday)
     day = input("Please input day of week: ").lower()
 
-    print('-'*40)
+    print('-'*100)
     return city, month, day
 
 
